d761677a59f8cf6783e86dd0ec4100e5df6d6ebc
Kirk.Wang <kirk.w.wang@gmail.com>
2019-03-15 19:05:52 +0800

    add config.go

---
+++ config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"github.com/lotteryjs/configor"
+)
+
+// Configuration is stuff that can be configured externally per env variables or config file (config.yml).
+type Configuration struct {
+	Server struct {
+		ListenAddr      string `default:""`
+		Port            int    `default:"80"`
+		ResponseHeaders map[string]string
+	}
+	Database struct {
+		Dialect    string `default:""`
+		Connection string `default:""`
+	}
+}
+
+// Get returns the configuration extracted from env variables or config file.
+func Get() *Configuration {
+	conf := new(Configuration)
+	err := configor.New(&configor.Config{EnvironmentPrefix: "TenMinutesApi"}).Load(conf, "config.yml", "/etc/gotify/config.yml")
+	if err != nil {
+		panic(err)
+	}
+	return conf
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gin-contrib/sse v0.0.0-20190301062529-5545eab6dad3 // indirect
 	github.com/gin-gonic/gin v1.3.0
 	github.com/golang/protobuf v1.3.1 // indirect
+	github.com/lotteryjs/configor v1.0.2
 	github.com/mattn/go-isatty v0.0.7 // indirect
 	github.com/stretchr/testify v1.3.0
 	github.com/ugorji/go/codec v0.0.0-20190309163734-c4a1c341dc93 // indirect
