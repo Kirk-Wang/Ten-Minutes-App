de9700ac03010b8fbc5f03769cd41f5c9274fda6
Kirk.Wang <kirk.w.wang@gmail.com>
2019-03-17 01:36:41 +0800

    update database.go

--- database/database.go
+++ database/database.go
@@ -1,9 +1,30 @@
 package database
 
 import (
-	"os"
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"time"
 )
 
-func New() {
+// New is
+func New(connection, dbname string) (*TenDatabase, error) {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		return nil, err
+	}
+	ctx, _ = context.WithTimeout(context.Background(), 2*time.Second)
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return nil, err
+	}
+	db := client.Database()
+	return &TenDatabase{DB: db}, nil
+}
 
+// TenDatabase is a wrapper for the gorm framework.
+type TenDatabase struct {
+	DB *mongo.Database
 }
