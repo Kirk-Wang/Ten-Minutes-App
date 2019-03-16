d0d335b9ad5bdeec05ab0556b488b8d21b74ca5e
Kirk.Wang <kirk.w.wang@gmail.com>
2019-03-17 02:18:01 +0800

    update user.go

--- database/user.go
+++ database/user.go
@@ -1,12 +1,29 @@
 package database
 
 import (
-	"go.mongodb.org/mongo-driver/mongo"
+	"context"
+
+	"github.com/lotteryjs/ten-minutes-api/model"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 // GetUsers returns all users.
-func (d *mongo.Database) GetUsers() []*model.User {
-	var users []*model.User
-	d.DB.Find(&users)
-	return users
+func (d *TenDatabase) GetUsers() []*model.User {
+	// var users []*model.User
+	// d.DB.Find(&users)
+	// return users
+	cursor, err := d.DB.Collection("users").Find(
+		context.Background(),
+		bson.D{},
+	)
+	defer cursor.Close(context.Background())
+
+	users := []*model.User
+	for cursor.Next(context.Background()) {
+		user := &model.User{}
+		if err := cursor.Decode(user); err != nil {
+			return nil, err
+		}
+		feedbacks = append(feedbacks, feedback)
+	}
 }
