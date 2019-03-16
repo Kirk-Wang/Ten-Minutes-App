54b79ecda9426269e725878a376a2f7fc386790b
Kirk.Wang <kirk.w.wang@gmail.com>
2019-03-16 21:13:30 +0800

    update user.go

--- go.mod
+++ go.mod
@@ -5,12 +5,17 @@ go 1.12
 require (
 	github.com/gin-contrib/sse v0.0.0-20190301062529-5545eab6dad3 // indirect
 	github.com/gin-gonic/gin v1.3.0
+	github.com/go-playground/locales v0.12.1 // indirect
+	github.com/go-playground/universal-translator v0.16.0 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/golang/protobuf v1.3.1 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
+	github.com/google/go-cmp v0.2.0 // indirect
+	github.com/leodido/go-urn v1.1.0 // indirect
 	github.com/lotteryjs/configor v1.0.2
 	github.com/mattn/go-isatty v0.0.7 // indirect
-	github.com/stretchr/testify v1.3.0
+	github.com/stretchr/testify v1.3.0 // indirect
+	github.com/tidwall/pretty v0.0.0-20180105212114-65a9db5fad51 // indirect
 	github.com/ugorji/go/codec v0.0.0-20190316083543-95c34d148dff // indirect
 	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
 	github.com/xdg/stringprep v1.0.0 // indirect
@@ -18,6 +23,7 @@ require (
 	golang.org/x/crypto v0.0.0-20190313024323-a1f597ede03a // indirect
 	golang.org/x/sync v0.0.0-20190227155943-e225da77a7e6 // indirect
 	golang.org/x/text v0.3.0 // indirect
-	gopkg.in/go-playground/validator.v8 v8.18.2
+	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
+	gopkg.in/go-playground/validator.v9 v9.27.0
 	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
---
+++ model/user.go
@@ -0,0 +1,59 @@
+package model
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// The User holds information about the credentials of a user and its application and client tokens.
+type User struct {
+	ID    primitive.ObjectID `bson:"_id"`
+	Name  string             `bson:"name"`
+	Pass  []byte             `bson:"pass"`
+	Admin bool               `bson:"admin"`
+}
+
+// UserExternal Model
+//
+// The User holds information about permission and other stuff.
+//
+// swagger:model User
+type UserExternal struct {
+	// The user id.
+	//
+	// read only: true
+	// required: true
+	// example: 25
+	ID uint `json:"id"`
+	// The user name. For login.
+	//
+	// required: true
+	// example: unicorn
+	Name string `validate:"required" json:"name" query:"name" form:"name"`
+	// If the user is an administrator.
+	//
+	// example: true
+	Admin bool `json:"admin" form:"admin" query:"admin"`
+}
+
+// UserExternalWithPass Model
+//
+// The UserWithPass holds information about the credentials and other stuff.
+//
+// swagger:model UserWithPass
+type UserExternalWithPass struct {
+	UserExternal
+	UserExternalPass
+}
+
+// UserExternalPass Model
+//
+// The Password for updating the user.
+//
+// swagger:model UserPass
+type UserExternalPass struct {
+	// The user password. For login.
+	//
+	// required: true
+	// example: nrocinu
+	Pass string `json:"pass,omitempty" form:"pass" query:"pass" validate:"required"`
+}
