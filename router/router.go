9c01706bdb259b76de97e9936681a3c3f7b1e84a
Kirk.Wang <kirk.w.wang@gmail.com>
2019-03-16 17:57:24 +0800

    update router.go

--- router/router.go
+++ router/router.go
@@ -1,12 +1,18 @@
 package router
 
 import (
-	"time"
-
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+
+	"github.com/lotteryjs/ten-minutes-api/config"
+	"github.com/lotteryjs/ten-minutes-api/model"
 )
 
 // Create creates the gin engine with all routes.
-func Create(db *database.GormDatabase, vInfo *model.VersionInfo, conf *config.Configuration) (*gin.Engine, func()) {
+func Create(db *mongo.Database, vInfo *model.VersionInfo, conf *config.Configuration) (*gin.Engine, func()) {
+	g := gin.New()
+
+	g.Use(gin.Logger(), gin.Recovery(), error.Handler(), location.Default())
+	g.NoRoute(error.NotFound())
 
 }
