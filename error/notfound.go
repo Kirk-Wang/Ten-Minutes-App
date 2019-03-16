b87a6b580a8e0e5f22c1ce522038066f93cc6201
Kirk.Wang <kirk.w.wang@gmail.com>
2019-03-16 18:11:45 +0800

    update notfound.go

---
+++ error/notfound.go
@@ -0,0 +1,19 @@
+package error
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lotteryjs/ten-minutes-api/model"
+)
+
+// NotFound creates a gin middleware for handling page not found.
+func NotFound() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, &model.Error{
+			Error:            http.StatusText(http.StatusNotFound),
+			ErrorCode:        http.StatusNotFound,
+			ErrorDescription: "page not found",
+		})
+	}
+}
