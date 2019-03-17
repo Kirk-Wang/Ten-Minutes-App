6289b41a19e8c6fb8bf2ae1f182dfbbf4739cd7f
Kirk.Wang <kirk.w.wang@gmail.com>
2019-03-17 18:59:31 +0800

    update database_test.go

--- database/database_test.go
+++ database/database_test.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 	"testing"
 )
@@ -12,4 +13,10 @@ func TestDatabaseSuite(t *testing.T) {
 type DatabaseSuite struct {
 	suite.Suite
 	db *TenDatabase
+}
+
+func (s *DatabaseSuite) BeforeTest(suiteName, testName string) {
+	db, err := New("mongodb://root:123456@localhost:27017", "typicode")
+	assert.Nil(s.T(), err)
+	s.db = db
 }
