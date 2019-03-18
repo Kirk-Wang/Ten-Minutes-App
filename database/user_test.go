ed76820a6f81217ba6aa3c4f696a6aad5d64d78d
Kirk.Wang <kirk.w.wang@gmail.com>
2019-03-18 17:48:20 +0800

    udpate user_test.go

--- database/user_test.go
+++ database/user_test.go
@@ -8,7 +8,28 @@ import (
 func (s *DatabaseSuite) TestUser() {
 	s.db.DB.Collection("users").Drop(nil)
 
-	kirk := (&model.User{Name: "kirk", Pass: []byte{1, 2, 3, 4}, Admin: true}).New()
+	kirk := (&model.User{
+		Name:     "Leanne Graham",
+		UserName: "Bret",
+		Email:    "Sincere@april.biz",
+		Address: model.UserAddress{
+			Street:  "Kulas Light",
+			Suite:   "Apt. 556",
+			City:    "Gwenborough",
+			Zipcode: "92998-3874",
+			Geo: model.UserAddressGeo{
+				Lat: "-37.3159",
+				Lng: "81.1496",
+			},
+		},
+		Phone:   "1-770-736-8031 x56442",
+		Website: "hildegard.org",
+		Company: model.UserCompany{
+			Name:        "Romaguera-Crona",
+			CatchPhrase: "Multi-layered client-server neural-net",
+			BS:          "harness real-time e-markets",
+		},
+	}).New()
 	err := s.db.CreateUser(kirk)
 	assert.Nil(s.T(), err)
 	assert.Equal(s.T(), kirk, s.db.GetUserByName("kirk"))
