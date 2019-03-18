405e0f293a56a3495ad3b8e9692bcff96c7da51d
Kirk.Wang <kirk.w.wang@gmail.com>
2019-03-18 15:24:12 +0800

    update database/user_test.go

--- database/user_test.go
+++ database/user_test.go
@@ -6,8 +6,11 @@ import (
 )
 
 func (s *DatabaseSuite) TestUser() {
+	s.db.DB.Collection("users").Drop(nil)
+
 	kirk := (&model.User{Name: "kirk", Pass: []byte{1, 2, 3, 4}, Admin: true}).New()
 	err := s.db.CreateUser(kirk)
 	assert.Nil(s.T(), err)
 	assert.Equal(s.T(), kirk, s.db.GetUserByName("kirk"))
+
 }
