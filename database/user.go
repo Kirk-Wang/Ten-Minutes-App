90b78c8f6339710ee78cc0525acf52164bca38b1
Kirk.Wang <kirk.w.wang@gmail.com>
2019-03-18 12:10:42 +0800

    update user_test.go

--- database/user.go
+++ database/user.go
@@ -32,7 +32,7 @@ func (d *TenDatabase) GetUsers() ([]*model.User, error) {
 
 // CreateUser creates a user.
 func (d *TenDatabase) CreateUser(user *model.User) error {
-	if _, err := d.DB.Collection("users").InsertOne(context.Background(), user); err != nil {
+	if _, err := d.DB.Collection("users").InsertOne(context.Background(), user.New()); err != nil {
 		return err
 	}
 	return nil
--- database/user_test.go
+++ database/user_test.go
@@ -1,9 +1,13 @@
 package database
 
-import "github.com/stretchr/testify/assert"
+import (
+	"github.com/lotteryjs/ten-minutes-api/model"
+	"github.com/stretchr/testify/assert"
+)
 
 func (s *DatabaseSuite) TestUser() {
-	users, _ := s.db.GetUsers()
-	s.T().Log(users)
-	assert.Len(s.T(), users, 1)
+	nicories := &model.User{Name: "kirk", Pass: []byte{1, 2, 3, 4}, Admin: true}
+	err := s.db.CreateUser(nicories)
+	// s.T().Log(err)
+	assert.Nil(s.T(), err)
 }
