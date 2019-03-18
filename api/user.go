6d8281da156751569749f3b5ae5abcb1af10dac3
Kirk.Wang <kirk.w.wang@gmail.com>
2019-03-18 18:11:28 +0800

    update api/user.go

--- api/user.go
+++ api/user.go
@@ -1,18 +1,30 @@
 package api
 
 import (
-	// "errors"
 	"github.com/gin-gonic/gin"
-	// "github.com/lotteryjs/ten-minutes-api/auth/password"
 	"github.com/lotteryjs/ten-minutes-api/model"
 )
 
 // The UserDatabase interface for encapsulating database access.
 type UserDatabase interface {
 	CreateUser(user *model.User) error
+	GetUsers() []*model.User
 }
 
 // The UserAPI provides handlers for managing users.
 type UserAPI struct {
 	DB UserDatabase
+}
+
+// GetUsers returns all the users
+func (a *UserAPI) GetUsers(ctx *gin.Context) {
+	// users := a.DB.GetUsers()
+
+	// var resp []*model.UserExternal
+	// for _, user := range users {
+	// 	resp = append(resp, toExternalUser(user))
+	// }
+
+	// ctx.JSON(200, resp)
+	ctx.JSON(200, "GetUsers")
 }
