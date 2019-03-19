0f0e16483618d0fada73bd06d0e5a6860424d535
Kirk.Wang <kirk.w.wang@gmail.com>
2019-03-19 18:58:09 +0800

    update router.go

--- router/router.go
+++ router/router.go
@@ -4,13 +4,14 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"github.com/lotteryjs/ten-minutes-api/api"
+	"github.com/lotteryjs/ten-minutes-api/config"
 	"github.com/lotteryjs/ten-minutes-api/database"
 	"github.com/lotteryjs/ten-minutes-api/error"
 	"github.com/lotteryjs/ten-minutes-api/model"
 )
 
 // Create creates the gin engine with all routes.
-func Create(db *database.TenDatabase, vInfo *model.VersionInfo) *gin.Engine {
+func Create(db *database.TenDatabase, vInfo *model.VersionInfo, conf *config.Configuration) *gin.Engine {
 	g := gin.New()
 
 	g.Use(gin.Logger(), gin.Recovery(), error.Handler())
@@ -20,7 +21,6 @@ func Create(db *database.TenDatabase, vInfo *model.VersionInfo) *gin.Engine {
 
 	g.Use(func(ctx *gin.Context) {
 		ctx.Header("Content-Type", "application/json")
-
 		for header, value := range conf.Server.ResponseHeaders {
 			ctx.Header(header, value)
 		}
