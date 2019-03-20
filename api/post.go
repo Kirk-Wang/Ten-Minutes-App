08ac8a21ee2d7f82cec0dd8cd156938454a2667c
Kirk.Wang <kirk.w.wang@gmail.com>
2019-03-20 19:38:57 +0800

    udpate post.go

---
+++ api/post.go
@@ -0,0 +1,52 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/lotteryjs/ten-minutes-api/model"
+	"strconv"
+)
+
+// The UserDatabase interface for encapsulating database access.
+type UserDatabase interface {
+	GetUsers(paging *model.Paging) []*model.User
+}
+
+// The UserAPI provides handlers for managing users.
+type UserAPI struct {
+	DB UserDatabase
+}
+
+// GetUsers returns all the users
+// _end=5&_order=DESC&_sort=id&_start=0 adapt react-admin
+func (a *UserAPI) GetUsers(ctx *gin.Context) {
+	var (
+		start int64
+		end   int64
+		sort  string
+		order int
+	)
+
+	start, _ = strconv.ParseInt(ctx.DefaultQuery("_start", "0"), 10, 64)
+	end, _ = strconv.ParseInt(ctx.DefaultQuery("_end", "10"), 10, 64)
+	sort = ctx.DefaultQuery("_sort", "_id")
+	order = 1
+
+	if sort == "id" {
+		sort = "_id"
+	}
+
+	if ctx.DefaultQuery("_order", "DESC") == "DESC" {
+		order = -1
+	}
+
+	users := a.DB.GetUsers(
+		&model.Paging{
+			Skip:    &start,
+			Limit:   &end,
+			SortKey: sort,
+			SortVal: order,
+		})
+
+	ctx.Header("X-Total-Count", strconv.Itoa(len(users)))
+	ctx.JSON(200, users)
+}
