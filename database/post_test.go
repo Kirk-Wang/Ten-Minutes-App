bdec502295e2da928e900a6d982accedfd99115d
Kirk.Wang <kirk.w.wang@gmail.com>
2019-03-20 19:16:32 +0800

    udpate database/post_test.go

--- database/post_test.go
+++ database/post_test.go
@@ -3,10 +3,20 @@ package database
 import (
 	"github.com/lotteryjs/ten-minutes-api/model"
 	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func (s *DatabaseSuite) TestPost() {
 	s.db.DB.Collection("posts").Drop(nil)
 
-	article := (&model.Post{}).new()
+	// user1
+	UserID, _ := primitive.ObjectIDFromHex("5c8f9a83da2c3fed4eee9dc1")
+	article := (&model.Post{
+		UserID: UserID,
+		Title:  "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+		Body:   "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+	}).New()
+
+	err := s.db.CreatePost(article)
+	assert.Nil(s.T(), err)
 }
