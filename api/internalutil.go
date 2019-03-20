87f24277fd83c03245ef435a81e31c7c260f7157
Kirk.Wang <kirk.w.wang@gmail.com>
2019-03-20 20:09:26 +0800

    update internalutil.go

--- api/internalutil.go
+++ api/internalutil.go
@@ -2,14 +2,14 @@ package api
 
 import (
 	"errors"
-	"strconv"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 
 	"github.com/gin-gonic/gin"
 )
 
-func withID(ctx *gin.Context, name string, f func(id uint)) {
-	if id, err := strconv.ParseUint(ctx.Param(name), 10, 32); err == nil {
-		f(uint(id))
+func withID(ctx *gin.Context, name string, f func(id primitive.ObjectID)) {
+	if id, err := primitive.ObjectIDFromHex(name); err == nil {
+		f(id)
 	} else {
 		ctx.AbortWithError(400, errors.New("invalid id"))
 	}
