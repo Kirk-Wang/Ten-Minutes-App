6e4cf406b515de226b1a205d44ff4f5f64b2955c
Kirk.Wang <kirk.w.wang@gmail.com>
2019-03-24 00:11:13 +0800

    udpate app.tsx add custom.d.ts

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,29 +1,11 @@
 import React, { Component } from 'react';
-import { Admin, Resource } from 'react-admin';
-import logo from './logo.svg';
-import './App.css';
+import { Admin, Resource, ListGuesser } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 
-export default App;
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+      <Resource name="users" list={ListGuesser} />
+  </Admin>
+)
---
+++ app/src/custom.d.ts
@@ -0,0 +1,3 @@
+
+declare module "ra-data-json-server"
+declare module "react-admin"
