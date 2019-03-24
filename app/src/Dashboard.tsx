30adab6b7889a5555e6b626f4b86268e50036a50
Kirk.Wang <kirk.w.wang@gmail.com>
2019-03-24 21:11:43 +0800

    update Dashboard.tsx

--- app/src/Dashboard.tsx
+++ app/src/Dashboard.tsx
@@ -5,7 +5,7 @@ import CardHeader from '@material-ui/core/CardHeader';
 
 export default () => (
     <Card>
-        <CardHeader title="haha" />
+        <CardHeader title="快速构建高质量的 React 应用" />
         <CardContent>guys</CardContent>
     </Card>
 );
