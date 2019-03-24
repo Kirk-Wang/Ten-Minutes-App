d2f94e5cc161cb989d3c6d493ebe06776d045444
Kirk.Wang <kirk.w.wang@gmail.com>
2019-03-24 21:42:51 +0800

    update Dashboard.tsx

--- app/src/Dashboard.tsx
+++ app/src/Dashboard.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
+import Avatar from '@material-ui/core/Avatar';
 
 export default () => (
     <Card>
         <CardHeader title="快速构建高质量的 React 应用" />
-        <CardContent>guys</CardContent>
+        <CardContent>
+          <Avatar alt="Material-UI" src="https://material-ui.com/static/images/material-ui-logo.svg" />
+        </CardContent>
     </Card>
 );
