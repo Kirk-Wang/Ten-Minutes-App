8f786262c900f95cc41c884e3f2ebabd16042400
Kirk.Wang <kirk.w.wang@gmail.com>
2019-03-24 20:53:47 +0800

    update App.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
-import PostIcon from '@material-ui/icons/Book';
-import { PostList, PostEdit, PostCreate } from './Posts';
 import jsonServerProvider from 'ra-data-json-server';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+import { PostList, PostEdit, PostCreate } from './Posts';
+import { UserList } from './Users';
 
 const dataProvider = jsonServerProvider("http://dev.admin.com:6868");
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
       <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+      <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
 )
 
