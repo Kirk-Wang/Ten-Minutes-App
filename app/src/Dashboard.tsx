49020c66595efa0a8c274a1341ee5b6a941f7958
Kirk.Wang <kirk.w.wang@gmail.com>
2019-03-24 21:02:17 +0800

    add Dashboard.tsx

---
+++ app/src/Dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+
+export default () => (
+    <Card>
+        <CardHeader title="haha" />
+        <CardContent>guys</CardContent>
+    </Card>
+);
