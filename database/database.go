e116a5269b998106c69481bb71cb97f8e4ed4801
Kirk.Wang <kirk.w.wang@gmail.com>
2019-03-29 03:03:54 +0800

    update database.go(timeout)

--- database/database.go
+++ database/database.go
@@ -10,13 +10,13 @@ import (
 
 // New creates a new wrapper for the mongo-go-driver.
 func New(connection, dbname string) (*TenDatabase, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(connection))
 	if err != nil {
 		return nil, err
 	}
-	ctxping, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	ctxping, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 	err = client.Ping(ctxping, readpref.Primary())
 	if err != nil {
