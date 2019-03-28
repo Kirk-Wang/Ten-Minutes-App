847f20db2e306414ec23214624721ca24093a1e1
Kirk.Wang <kirk.w.wang@gmail.com>
2019-03-29 03:24:12 +0800

    update database.go

--- database/database.go
+++ database/database.go
@@ -4,24 +4,24 @@ import (
 	"context"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
+	// "go.mongodb.org/mongo-driver/mongo/readpref"
 	"time"
 )
 
 // New creates a new wrapper for the mongo-go-driver.
 func New(connection, dbname string) (*TenDatabase, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(connection))
 	if err != nil {
 		return nil, err
 	}
-	ctxping, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	defer cancel()
-	err = client.Ping(ctxping, readpref.Primary())
-	if err != nil {
-		return nil, err
-	}
+	// ctxping, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	// defer cancel()
+	// err = client.Ping(ctxping, readpref.Primary())
+	// if err != nil {
+	// 	return nil, err
+	// }
 	db := client.Database(dbname)
 	return &TenDatabase{DB: db, Client: client, Context: ctx}, nil
 }
