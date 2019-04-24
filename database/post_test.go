a1ea24b73deaaa6f65b64f089bab313b6f95385d
Kirk.Wang <kirk.w.wang@gmail.com>
2019-04-24 23:39:19 +0800

    update post_test.go & add TestCreatePost

---
+++ database/post_test.go
@@ -0,0 +1,22 @@
+package database
+import (
+	"github.com/lotteryjs/ten-minutes-app/model"
+	"github.com/stretchr/testify/assert"
+)
+
+func (s *DatabaseSuite) TestCreatePost() {
+	s.db.DB.Collection("posts").Drop(nil)
+
+	user := s.db.GetUserByName("Graham")
+
+	article := (&model.Post{
+		UserID: user.ID,
+		Title:  "title1",
+		Body:   "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+	}).New()
+	
+	s.db.CreatePost(article)
+	post := s.db.GetPostByID(article.ID)
+	
+	assert.Equal(s.T(), article, post)
+}
