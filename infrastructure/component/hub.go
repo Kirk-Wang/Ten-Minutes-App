ad45f86c499a1d25c18c554b1c0ffb6e035239ac
Kirk.Wang <kirk.w.wang@gmail.com>
2019-07-12 03:19:41 +0800

    update Register()(hub.go)

--- infrastructure/component/hub.go
+++ infrastructure/component/hub.go
@@ -1,12 +1,25 @@
 package component
 
+import (
+	log "github.com/sirupsen/logrus"
+)
+
 type Components struct {
 	nonBlockingComponents []Component
 	blockingComponents    []Component
 }
 
 // Register registers a component to nonBlockingComponents or blockingComponents
-func (cs *Components) Register(nonBlocking, blocking Component) {}
+func (cs *Components) Register(component Component) {
+	if component.StartBlocking() {
+		cs.blockingComponents = append(cs.blockingComponents, component)
+	} else {
+		cs.nonBlockingComponents = append(cs.nonBlockingComponents, component)
+	}
+
+	typ := reflect.TypeOf(component)
+	log.Infof("Registers a component: %s", typ.String())
+}
 
 // List returns all components
 func (cs *Components) List() []Components {
